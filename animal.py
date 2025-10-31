a05fde28c580aade4a55f3e86b4cb7d7e585b4a7
JBT13 <jeremiahborjas@gmail.com>
2025-10-31 14:05:25 +0000

    2

--- animal.py
+++ animal.py
@@ -1,5 +1,5 @@
 from island import Island
-
+import random
 
 class Animal:
     NOT_FOUND = 0
@@ -34,6 +34,7 @@ class Animal:
         """
 
         offset = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
+        random.shuffle(offset)
         result = Animal.NOT_FOUND
 
         for dx, dy in offset:
